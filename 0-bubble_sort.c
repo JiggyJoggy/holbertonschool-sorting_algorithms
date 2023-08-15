eb9578cbe24b6f2766e09feb74f745068e0cde58
root <JaggerVanW@gmail.com>
2023-08-14 19:06:13 -0700

    Bubble Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 	int sorted = 0, temp;
 	size_t index;
 
-	if (!array !! size < 2)
+	if (!array || size < 2)
 	{
 		sorted = 1;
 		for (index = 0; index < size - 1; index++)
