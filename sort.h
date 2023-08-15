d476c4e8df6abc0c5888261fb61438aa263cc35c
root <JaggerVanW@gmail.com>
2023-08-14 19:08:31 -0700

    Bubble Sort

--- sort.h
+++ sort.h
@@ -3,6 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 
 #endif
